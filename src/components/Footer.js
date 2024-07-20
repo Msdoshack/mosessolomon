1edbccf70a8cac954a059b9c608e37259ce7c0c8
Msdoshack <msdosdarapper@gmail.com>
2024-07-20 04:00:50 +0100

    fix bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -56,8 +56,8 @@ const Footer = () => {
               </p>
 
               <p className="flex items-center gap-1">
-                Linkedin :
-                {/* <LinkedInIcon className="w-3 h-3" width={4} />: */}
+                Linkedin
+                <LinkedInIcon className="w-4 h-4" width={4} />:
                 <Link
                   target="_blank"
                   href={"https://www.linkedin.com/in/solomon-moses-4981b4105"}
