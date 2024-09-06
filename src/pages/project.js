93f2a4ea2654f971c5a04ac8c1906688e9f40978
Msdoshack <msdosdarapper@gmail.com>
2024-09-06 21:34:50 +0100

    added a project

--- src/pages/project.js
+++ src/pages/project.js
@@ -2,7 +2,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import React from "react";
-import project1 from "../../public/images/projects/agency-website-cover-image.jpg";
+import project1 from "../../public/images/projects/mysnippet.jpg";
 import project2 from "../../public/images/projects/crypto-screener-cover-image.jpg";
 import project3 from "../../public/images/projects/fashion-studio-website.jpg";
 import project4 from "../../public/images/projects/portfolio-cover-image.jpg";
@@ -22,18 +22,16 @@ const Projects = () => {
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
           <AnimatedText
-            Text="dream and achieve"
+            Text="dream to achieve"
             classname="!mb-16 text-7xl lg:text-6xl md:!text-5xl sm:!text-4xl"
           />
 
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
-                title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-                local currency."
-                link="#"
+                title="MySnippets"
+                summary="A minimalistic web application for safely storing your reusable snippets. It was built using standard Go libraries, plain JavaScript, HTML, and CSS, with PostgreSQL for the database."
+                link="https://mysnippets.onrender.com"
                 img={project1}
                 github="#"
                 type="featured project"
